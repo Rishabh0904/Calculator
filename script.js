f3451ed630e7b2ffe64e84be2953de633db3d017
Rishabh0904 <43871647+Rishabh0904@users.noreply.github.com>
2021-11-29 02:22:28 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// var btn = document.querySelector(".hs");
+// function myScript(e){
+//     e.preventDefault();
+//     console.log("clicked!!!");
+// }
+// btn.addEventListener("click", myScript);
