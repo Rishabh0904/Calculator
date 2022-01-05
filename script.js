d51ed917fa171af0145485272b519ab4a40c210a
Rishabh0904 <43871647+Rishabh0904@users.noreply.github.com>
2022-01-05 12:34:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,15 @@
-// var btn = document.querySelector(".hs");
-// function myScript(e){
-//     e.preventDefault();
-//     console.log("clicked!!!");
-// }
-// btn.addEventListener("click", myScript);
-
 var flag = true;
 var extend = document.querySelector("#extend");
-function myScript(e) {
+function add(e) {
     e.preventDefault();
     console.log(e);
     if (flag == false) {
         flag = true;
-        e.currentTarget.children[0].innerText = "v";
+        e.currentTarget.children[0].innerText = "";
         extend.nextSibling.parentNode.removeChild(extend.nextSibling);
     } else {
 
-        e.currentTarget.children[0].innerText = "dvksd";
+        e.currentTarget.children[0].innerText = "";
         console.log("clicked!!!");
         var divRow = document.createElement("div");
         divRow.classList.add("row");
@@ -29,7 +22,48 @@ function myScript(e) {
             console.log("hello");
             extend.parentNode.insertBefore(divRow, extend.nextSibling);
             flag = false;
+            let buttons = document.querySelectorAll(".col");
+            for (button of buttons) {
+                button.addEventListener('click', calculation)
+            }
         }
     }
 }
-extend.addEventListener("click", myScript);
+extend.addEventListener("click", add);
+let ans = '';
+function calculation(e){
+    buttonText = e.target.innerText;
+    if(buttonText == 'C'){
+        screen.value = "";
+        ans = "";
+        console.log("hello");
+    }else if(buttonText == "="){
+        screen.value = eval(ans);
+        ans = screen.value;
+
+    }else if(buttonText == "sin"){
+        ans += buttonText;
+        screen.value = ans;
+        if(buttonText == "("){
+            let str = '';
+            while(buttonText != ")"){
+                str = str + buttonText;
+            }
+            ans += buttonText;
+            screen.value = ans;
+            let val = Math.sin(str)
+            screen.value = val;
+        }
+
+    }else{
+        ans += buttonText;
+        screen.value = ans
+    }
+    // console.log(buttonText);
+}
+
+let screen = document.querySelector(".text");
+let buttons = document.querySelectorAll(".col");
+for (button of buttons) {
+    button.addEventListener('click', calculation)
+}
